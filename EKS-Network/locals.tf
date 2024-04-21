3660e8f176a72bb504772fa205cf8a2af5d9041a
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-04-21 18:46:36 +0200

    EKS VPC

--- EKS-Network/locals.tf
+++ EKS-Network/locals.tf
@@ -4,6 +4,6 @@
 #tolist() function is used to convert a set to a list order to access the elements by index
 # % is the modulo operator, which ensures that the index stays within the bounds of the availability zones list. If the index exceeds the length of the availability zones list, it wraps around to the beginning.
 locals {
-  private_az_index = element(var.az, index(tolist(toset(var.private_ciders)), each.key) % length(var.az))
-  public_az_index  = element(var.az, index(tolist(toset(var.public_ciders)), each.key) % length(var.az))
+  private_az_index = { for cidr in var.private_ciders : cidr => element(var.az, index(tolist(toset(var.private_ciders)), cidr) % length(var.az)) }
+  public_az_index  = { for cidr in var.public_ciders : cidr => element(var.az, index(tolist(toset(var.public_ciders)), cidr) % length(var.az)) }
 }
