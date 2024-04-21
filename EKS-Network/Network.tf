883d7b74263dce2cdd25ef5543471263ffdd3a32
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-04-21 13:04:27 +0200

    EKS VPC

--- EKS-Network/Network.tf
+++ EKS-Network/Network.tf
@@ -37,18 +37,18 @@ resource "aws_internet_gateway_attachment" "EKS_internet_gateway_attachment" {
   vpc_id              = aws_vpc.EKS_VPC.id
 }
 
-resource "aws_eip" "eip" {
-  domain     = "vpc"
-  depends_on = [aws_internet_gateway.EKS_internet_gateway]
-}
+# resource "aws_eip" "eip" {
+#   domain     = "vpc"
+#   depends_on = [aws_internet_gateway.EKS_internet_gateway]
+# }
 
 
-resource "aws_nat_gateway" "EKS_Nat_gateway" {
-  allocation_id = aws_eip.eip.id
-  subnet_id     = element(aws_subnet.public_subnets[*].id, 0)
-  tags          = var.my_tags
-  depends_on    = [aws_eip.eip]
-}
+# resource "aws_nat_gateway" "EKS_Nat_gateway" {
+#   allocation_id = aws_eip.eip.id
+#   subnet_id     = element(aws_subnet.public_subnets[*].id, 0)
+#   tags          = var.my_tags
+#   depends_on    = [aws_eip.eip]
+# }
 
 
 
@@ -64,15 +64,15 @@ resource "aws_route_table" "private_route_table" {
 
 
 
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.EKS_VPC.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.EKS_internet_gateway.id
-  }
-  tags       = var.my_tags
-  depends_on = [aws_nat_gateway.EKS_Nat_gateway]
-}
+# resource "aws_route_table" "public_route_table" {
+#   vpc_id = aws_vpc.EKS_VPC.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.EKS_internet_gateway.id
+#   }
+#   tags       = var.my_tags
+#   depends_on = [aws_nat_gateway.EKS_Nat_gateway]
+# }
 
 
 
@@ -83,11 +83,11 @@ resource "aws_route_table_association" "private_route_table_association" {
   route_table_id = aws_route_table.private_route_table.id
 }
 
-resource "aws_route_table_association" "public_route_table_association" {
-  for_each       = aws_subnet.public_subnets
-  subnet_id      = each.value.id
-  route_table_id = aws_route_table.private_route_table.id
-}
+# resource "aws_route_table_association" "public_route_table_association" {
+#   for_each       = aws_subnet.public_subnets
+#   subnet_id      = each.value.id
+#   route_table_id = aws_route_table.private_route_table.id
+# }
 
 
 
