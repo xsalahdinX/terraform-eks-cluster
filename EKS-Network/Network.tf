6e0c2ab9bf39be2196e81c67f1070f1824fceb17
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-04-21 13:36:44 +0200

    EKS VPC

--- EKS-Network/Network.tf
+++ EKS-Network/Network.tf
@@ -20,7 +20,7 @@ resource "aws_subnet" "private_subnets" {
 resource "aws_subnet" "public_subnets" {
   for_each          = toset(var.public_ciders)
   vpc_id            = aws_vpc.EKS_VPC.id
-  availability_zone = element(var.az, length(each.key))
+  availability_zone = element(var.az, length(var.public_ciders) % length(var.az))
   cidr_block        = each.key
   tags              = var.my_tags
 
@@ -32,23 +32,19 @@ resource "aws_internet_gateway" "EKS_internet_gateway" {
   tags   = var.my_tags
 }
 
-resource "aws_internet_gateway_attachment" "EKS_internet_gateway_attachment" {
-  internet_gateway_id = aws_internet_gateway.EKS_internet_gateway.id
-  vpc_id              = aws_vpc.EKS_VPC.id
+
+resource "aws_eip" "eip" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.EKS_internet_gateway]
 }
 
-# resource "aws_eip" "eip" {
-#   domain     = "vpc"
-#   depends_on = [aws_internet_gateway.EKS_internet_gateway]
-# }
 
-
-# resource "aws_nat_gateway" "EKS_Nat_gateway" {
-#   allocation_id = aws_eip.eip.id
-#   subnet_id     = element(aws_subnet.public_subnets[*].id, 0)
-#   tags          = var.my_tags
-#   depends_on    = [aws_eip.eip]
-# }
+resource "aws_nat_gateway" "EKS_Nat_gateway" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = element(aws_subnet.public_subnets[*].id, 0)
+  tags          = var.my_tags
+  depends_on    = [aws_eip.eip]
+}
 
 
 
@@ -64,15 +60,15 @@ resource "aws_route_table" "private_route_table" {
 
 
 
-# resource "aws_route_table" "public_route_table" {
-#   vpc_id = aws_vpc.EKS_VPC.id
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.EKS_internet_gateway.id
-#   }
-#   tags       = var.my_tags
-#   depends_on = [aws_nat_gateway.EKS_Nat_gateway]
-# }
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.EKS_VPC.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.EKS_internet_gateway.id
+  }
+  tags       = var.my_tags
+  depends_on = [aws_nat_gateway.EKS_Nat_gateway]
+}
 
 
 
@@ -83,11 +79,11 @@ resource "aws_route_table_association" "private_route_table_association" {
   route_table_id = aws_route_table.private_route_table.id
 }
 
-# resource "aws_route_table_association" "public_route_table_association" {
-#   for_each       = aws_subnet.public_subnets
-#   subnet_id      = each.value.id
-#   route_table_id = aws_route_table.private_route_table.id
-# }
+resource "aws_route_table_association" "public_route_table_association" {
+  for_each       = aws_subnet.public_subnets
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private_route_table.id
+}
 
 
 
