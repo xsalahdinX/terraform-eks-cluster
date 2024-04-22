2416235754cf7c411a869ad5dde35085d2c8d5e2
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-04-22 15:08:57 +0200

    EKS VPC

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "EKS_Network" {
   vpc_cider      = "10.0.0.0/16"
   private_ciders = ["10.0.1.0/24", "10.0.2.0/24"]
   public_ciders  = ["10.0.3.0/24", "10.0.4.0/24"]
-  my_tags        = { "Name" : "EKS Resource", "Owned" : "Salahdin" }
+  my_tags        = { "Owned" : "Salahdin&Gamil"}
   az             = ["us-east-1a", "us-east-1b"]
 
 }
