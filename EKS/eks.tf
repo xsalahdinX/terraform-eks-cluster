c2c26e645657b9346d6b3d2ba35e376fd1ea5fc9
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-04-25 18:04:20 +0200

    Node group

--- EKS/eks.tf
+++ EKS/eks.tf
@@ -31,3 +31,8 @@ output "endpoint" {
 output "kubeconfig-certificate-authority-data" {
   value = aws_eks_cluster.Eks_Cluster.certificate_authority[0].data
 }
+
+output "cluster_name" {
+  value = aws_eks_cluster.Eks_Cluster.name
+  
+}
