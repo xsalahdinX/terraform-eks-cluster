a749181c1365224a53643a141d8047de4b8f87e8
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-05-02 12:23:18 +0300

    adding aws auth

--- aws-auth/provider.tf
+++ aws-auth/provider.tf
@@ -9,3 +9,13 @@ provider "helm" {
     }
   }
 }
+
+provider "kubernetes" {
+    host                   = var.host
+    cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      command     = "aws"
+    }
+  }
