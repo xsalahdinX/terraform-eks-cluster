e423117c13f5c2f5c1ab45679d4170ef52a4bea0
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-05-02 16:21:49 +0300

    adding aws auth

--- aws-auth/main.tf
+++ aws-auth/main.tf
@@ -1,4 +1,14 @@
 resource "helm_release" "aws_auth" {
   name       = "aws-auth"
   chart      = "./charts/aws-auth"
+
+  set {
+    name   = "accountId"
+    value = "${output.account_id}"
+ }
+  set {
+    name   = "nodegroup_role_arn"
+    value = "arn:aws:iam::${output.account_id}:role/node-group-role"
+ }
+
 }
--- aws-auth/provider.tf
+++ aws-auth/provider.tf
@@ -4,18 +4,8 @@ provider "helm" {
     cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name ,"--role-arn", "arn:aws:iam::211125357951:role/eks_access_role"]
       command     = "aws"
     }
   }
 }
-
-provider "kubernetes" {
-    host                   = var.host
-    cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-      command     = "aws"
-    }
-  }
