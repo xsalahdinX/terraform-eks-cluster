80dfc38024edc6716d77c615e3b87707b294ad5e
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-05-07 10:59:45 +0300

    adding aws auth

--- aws-auth/provider.tf
+++ aws-auth/provider.tf
@@ -11,19 +11,19 @@
 #   }
 # }
 
-provider "helm" {
-    alias = "helm_release"
-  kubernetes {
-    host                   = var.host
-    cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
-    token                  = data.aws_eks_cluster_auth.eks.token
+# provider "helm" {
+#     alias = "helm_release"
+#   kubernetes {
+#     host                   = var.host
+#     cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+#     token                  = data.aws_eks_cluster_auth.eks.token
 
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "kubectl"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["delete", "cm", "aws-auth", "kube-system"]
-  }
+#   exec {
+#     api_version = "client.authentication.k8s.io/v1beta1"
+#     command     = "kubectl"
+#     # This requires the awscli to be installed locally where Terraform is executed
+#     args = ["delete", "cm", "aws-auth", "kube-system"]
+#   }
 
-  }
-}
+#   }
+# }
