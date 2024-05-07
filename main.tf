06c848b0d8ba0b3829c97a4ffeb58362c5792975
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-05-07 11:20:47 +0300

    adding aws auth

--- main.tf
+++ main.tf
@@ -53,9 +53,9 @@ module "cluser_role" {
   depends_on = [module.EKS]  
 }
 
-module "aws_auth" {
-  source = "./aws-auth"
-  cluster_name = module.EKS.cluster_name
-  cluster_ca_certificate = module.EKS.kubeconfig-certificate-authority-data
-  host = module.EKS.endpoint
-}
+# module "aws_auth" {
+#   source = "./aws-auth"
+#   cluster_name = module.EKS.cluster_name
+#   cluster_ca_certificate = module.EKS.kubeconfig-certificate-authority-data
+#   host = module.EKS.endpoint
+# }
