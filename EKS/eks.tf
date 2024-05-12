e990ba568b427a33ae9fea26b7ec02fe81248858
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-05-12 18:17:41 +0300

    add efs  controller deployment

--- EKS/eks.tf
+++ EKS/eks.tf
@@ -15,17 +15,10 @@ resource "aws_eks_cluster" "Eks_Cluster" {
     bootstrap_cluster_creator_admin_permissions = true
   }
 
-#   Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-#   Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
     aws_iam_role_policy_attachment.EKS_AmazonEKSClusterPolicy,
     aws_iam_role_policy_attachment.EKS_AmazonEKSVPCResourceController,
   ]
-}
-
-
-data "tls_certificate" "EKS_cert" {
-  url = aws_eks_cluster.Eks_Cluster.identity[0].oidc[0].issuer
 }
 
 resource "aws_iam_openid_connect_provider" "eks-cluster-oidc" {
@@ -34,24 +27,3 @@ resource "aws_iam_openid_connect_provider" "eks-cluster-oidc" {
   url             = data.tls_certificate.EKS_cert.url
 }
 
-
-
-
-
-
-output "endpoint" {
-  value = aws_eks_cluster.Eks_Cluster.endpoint
-}
-
-output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.Eks_Cluster.certificate_authority[0].data
-}
-
-output "cluster_name" {
-  value = aws_eks_cluster.Eks_Cluster.name
-  
-}
-
-output "cluster_arn" {
-  value = aws_eks_cluster.Eks_Cluster.arn 
-}
