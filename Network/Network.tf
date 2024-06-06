1b8e3adf56c68456a9817fe4b27b52d00724deba
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-06-06 16:38:39 +0300

    adjust networl module

--- Network/Network.tf
+++ Network/Network.tf
@@ -43,7 +43,7 @@ resource "aws_eip" "eip" {
 # NAT Gateway configuration
 resource "aws_nat_gateway" "EKS_Nat_gateway" {
   for_each = aws_subnet.public_subnets
-  allocation_id = aws_eip.eip[each.key].id
+  allocation_id = count.index < length(aws_eip.eip) ? aws_eip.eip[count.index].id : null
   subnet_id     = each.value.id
   tags          = merge({ "Name" : "EKS_Nat_gateway" }, var.my_tags)
   depends_on    = [aws_eip.eip]
