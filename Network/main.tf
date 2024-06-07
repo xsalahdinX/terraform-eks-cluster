d84285e0ac9f21c2f2d9dcaefcd9c44f92610734
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-06-07 16:19:14 +0300

    adjust networl module

--- Network/main.tf
+++ Network/main.tf
@@ -89,7 +89,7 @@ resource "aws_route_table" "public_route_table" {
 resource "aws_route_table_association" "private_route_table_association" {
   for_each       = aws_subnet.private_subnets
   subnet_id      = each.value.id
-  route_table_id = each.value.id
+  route_table_id = aws_route_table.private_route_table[each.value.id].id
 }
 
 resource "aws_route_table_association" "public_route_table_association" {
