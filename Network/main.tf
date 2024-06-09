b6ca72f9e2b1e5ddcc7b47af12200f7e01575158
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-06-09 23:16:19 +0300

    adjust networl module

--- Network/main.tf
+++ Network/main.tf
@@ -69,17 +69,12 @@ resource "aws_route" "private_route" {
 
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.EKS_VPC.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.EKS_internet_gateway.id
-  }
   tags = merge({ "Name" : "EKS_public_route_table" }, var.my_tags)
-
   depends_on = [aws_nat_gateway.EKS_Nat_gateway]
 }
 
 
-resource "aws_route" "private_route" {
+resource "aws_route" "public_route" {
   route_table_id         = aws_route_table.public_route_table.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.EKS_internet_gateway.id
